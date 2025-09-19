61094da83b475c718c979311d71e55c8e87ac52d
SL <81534940+sjl-03@users.noreply.github.com>
2025-09-19 15:28:05 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int WINDOW_WIDTH = 384;
+    const int WINDOW_HEIGHT = 384;
+    
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Top down");
+
+    Texture2D map = LoadTexture("nature_tileset/mymap.png");
+    float mapX {};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        Vector2 mapPos {mapX, 0.0};
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        
+        EndDrawing();
+    }
+    UnloadTexture(map);
+    CloseWindow();
+
+    return 0;
+}
