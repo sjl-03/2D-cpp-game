612bae36413f378e8e0719f1a4fa2f897ebfdac0
SL <81534940+sjl-03@users.noreply.github.com>
2025-09-20 09:19:25 +0100

    added character

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@ int main(void)
 
     Texture2D map = LoadTexture("nature_tileset/mymap.png");
     Vector2 mapPos {0.0, 0.0};
-    float speed {4};
+    float speed {4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    int numImageInSpriteKnight = 6;
+    float scaleKnight = 4;
+    Vector2 knightPos{
+        (float)WINDOW_WIDTH/2.0f - scaleKnight*(0.5f* (float)knight.width / (float)numImageInSpriteKnight),
+        (float)WINDOW_HEIGHT/2.f - scaleKnight*(0.5f* (float)knight.height)
+    };
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -28,7 +36,13 @@ int main(void)
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
 
+        // Draw map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        // Draw character
+        Rectangle knightSource{0.f, 0.f,  (float)knight.width/(float)numImageInSpriteKnight,  (float)knight.height};
+        Rectangle knightDest{knightPos.x, knightPos.y, scaleKnight*(float)knightSource.width, scaleKnight*(float)knightSource.height};
+        Vector2 knightOrigin {};
+        DrawTexturePro(knight,knightSource, knightDest, knightOrigin, 0.f, WHITE);
         
         EndDrawing();
     }
